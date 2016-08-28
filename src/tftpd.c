44e51930c5ed8cbee1f080421b8c03a538a48f9b
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2016-08-28 00:18:36 +0000

    Argument parsed

--- src/tftpd.c
+++ src/tftpd.c
@@ -1,6 +1,16 @@
-/* your code goes here. */
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	return 0;
+    if (argc < 3) {
+        printf("Two arguments required: tftpd [port] [dir]\n");
+        exit(1);
+    }
+    
+    int port = atoi(argv[1]);
+    char *dir = argv[2];
+
+    printf("port: %i\n dir: %s\n", port, dir);
+    return 0;
 }
