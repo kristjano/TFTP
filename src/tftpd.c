dd4377a0892af38c328f6626d6a4f4cd0c2b0bea
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2016-08-30 14:19:38 +0000

    Server receives messages through UDP socket

--- src/tftpd.c
+++ src/tftpd.c
@@ -1,16 +1,56 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
     if (argc < 3) {
-        printf("Two arguments required: tftpd [port] [dir]\n");
+        printf("Usage: tftpd [port] [dir]\n");
         exit(1);
     }
-    
-    int port = atoi(argv[1]);
-    char *dir = argv[2];
 
-    printf("port: %i\n dir: %s\n", port, dir);
+    int port = atoi(argv[1]); // port number from program arguments
+    char *dir = argv[2]; // directory path from program arguments
+    int sockfd;
+    struct sockaddr_in server, client;
+    char message[512];
+
+    /* Create and bind a UDP socket */
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+
+    /* Network functions need arguments in network byte order instead
+     * of host byte order. The macros htonl, htons convert the
+     * values.
+     */
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(port);
+    bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+
+    for (;;) {
+        /* Receive up to one byte less than declared, because it will
+         * be NUL-terminated later.
+         */
+        socklen_t len = (socklen_t) sizeof(client);
+        ssize_t n = recvfrom(sockfd, message, sizeof(message) - 1,
+                             0, (struct sockaddr *) &client, &len);
+
+        message[n] = '\0';
+        fprintf(stdout, "Received:\n%s\n", message);
+        fflush(stdout);
+
+        /* convert message to upper case. */
+        for (int i = 0; i < n; ++i) {
+            message[i] = toupper(message[i]);
+        }
+
+        sendto(sockfd, message, (size_t) n, 0,
+               (struct sockaddr *) &client, len);
+    }
     return 0;
 }
