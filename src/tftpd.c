8264842a3f3b4e502170520511a13980bd58874a
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2016-08-30 15:15:25 +0000

    checks opcode and calls correct function

--- src/tftpd.c
+++ src/tftpd.c
@@ -5,6 +5,13 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+
+int RRQ();
+int WRQ();
+int DATA();
+int ACK();
+int ERROR();
 
 int main(int argc, char *argv[])
 {
@@ -15,9 +22,12 @@ int main(int argc, char *argv[])
 
     int port = atoi(argv[1]); // port number from program arguments
     char *dir = argv[2]; // directory path from program arguments
-    int sockfd;
+    int sockfd, dirfd;
     struct sockaddr_in server, client;
-    char message[512];
+    char message[516];
+
+    // Open directory to share
+    // TODO
 
     /* Create and bind a UDP socket */
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -41,7 +51,35 @@ int main(int argc, char *argv[])
                              0, (struct sockaddr *) &client, &len);
 
         message[n] = '\0';
-        fprintf(stdout, "Received:\n%s\n", message);
+        
+        int opcode = message[1];
+        char filename[512];
+        
+        switch (opcode)
+        {
+            case 1:
+                RRQ();
+                break;
+            case 2:
+                WRQ();
+                break;
+            case 3:
+                RRQ();
+                break;
+            case 4:
+                RRQ();
+                break;
+            case 5:
+                RRQ();
+                break;
+            default:
+                ERROR();
+        }
+        
+        sprintf(filename, "%s", &message[2]);
+        
+        fprintf(stdout, "Received:\n%s\n", filename);
+        
         fflush(stdout);
 
         /* convert message to upper case. */
@@ -52,5 +90,36 @@ int main(int argc, char *argv[])
         sendto(sockfd, message, (size_t) n, 0,
                (struct sockaddr *) &client, len);
     }
+    return 0;
+}
+
+int RRQ()
+{
+    printf("RRQ\n");
+    return 0;
+}
+
+int WRQ()
+{
+    // do nothing
+    printf("WRQ\n");
+    return 0;
+}
+
+int DATA()
+{
+    printf("DATA\n");
+    return 0;
+}
+
+int ACK()
+{
+    printf("ACK\n");
+    return 0;
+}
+
+int ERROR()
+{
+    printf("ERROR\n");
     return 0;
 }
