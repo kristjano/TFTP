9e469173b09d276fe7929ae5cf4896ea222628d2
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2016-09-06 15:19:31 +0000

    Sends first data packet

--- src/tftpd.c
+++ src/tftpd.c
@@ -8,24 +8,34 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int RRQ(char message[]);
+int RRQ(char message[], int sockfd);
 int WRQ();
 int DATA();
 int ACK();
 int ERROR();
 
+int data_transfer(int fd, int sockfd);
+
+// struct DATA {
+//     unsigned short int op;
+//     unsigned short int block;
+//     char data[512];
+// };
+
+struct sockaddr_in server, client;
+
 int main(int argc, char *argv[])
 {
+    int port = atoi(argv[1]); // port number from program arguments
+    char *dir = argv[2]; // directory path from program arguments
+    int sockfd;
+    char message[516];
+
+    /* Check usage */
     if (argc < 3) {
         printf("Usage: tftpd [port] [dir]\n");
         exit(1);
     }
-
-    int port = atoi(argv[1]); // port number from program arguments
-    char *dir = argv[2]; // directory path from program arguments
-    int sockfd;
-    struct sockaddr_in server, client;
-    char message[516];
     
     /* Change directory */
     if (chdir(dir) == -1)
@@ -59,11 +69,10 @@ int main(int argc, char *argv[])
         
         int opcode = message[1];
         
-        
         switch (opcode)
         {
             case 1:
-                RRQ(message);
+                RRQ(message, sockfd);
                 break;
             case 2:
                 WRQ();
@@ -81,23 +90,12 @@ int main(int argc, char *argv[])
                 // Illegal opcode. Do something.
                 break;
         }
-        
-        
-        
         fflush(stdout);
-
-        /* convert message to upper case. */
-        for (int i = 0; i < n; ++i) {
-            message[i] = toupper(message[i]);
-        }
-
-        sendto(sockfd, message, (size_t) n, 0,
-               (struct sockaddr *) &client, len);
     }
     return 0;
 }
 
-int RRQ(char message[])
+int RRQ(char message[], int sockfd)
 {
     //        2 bytes    string   1 byte     string   1 byte
     //        -----------------------------------------------
@@ -106,14 +104,27 @@ int RRQ(char message[])
     printf("RRQ\n");
     
     char filename[512], mode[512];
+    int fd;
+    //struct DATA datagram;
     sprintf(filename, "%s", &message[2]);
     sprintf(mode, "%s", &message[3 + strlen(filename)]);
     
     fprintf(stdout, "Filename:\n%s\n", filename);
     fprintf(stdout, "Mode:\n%s\n", mode);
     
-    int fd = open(filename, O_RDONLY, S_IRUSR);
-    fprintf(stdout, "%d", fd);
+    /* Open the file to send */
+    if ((fd = open(filename, O_RDONLY, S_IRUSR)) == -1) {
+        fprintf(stderr, "File does not exist or is not accessable");
+        exit(1);
+    }
+
+    data_transfer(fd, sockfd);
+    
+    if (close(fd) == -1) {
+        fprintf(stderr, "Error when closing file.");
+        exit(1);
+    }
+
     return 0;
 }
 
@@ -126,6 +137,7 @@ int WRQ()
 
 int DATA()
 {
+    // do nothing
     printf("DATA\n");
     return 0;
 }
@@ -139,5 +151,34 @@ int ACK()
 int ERROR()
 {
     printf("ERROR\n");
+    return 0;
+}
+
+int data_transfer(int fd, int sockfd)
+{
+    /* Build the datagram to send */
+    // datagram.op = 3;
+    // datagram.block = 1;
+    // read(fd, datagram.data, sizeof(datagram.data));
+    unsigned short int opcode = htons(3);
+    unsigned short int blockno = htons(1);
+    char data[512];
+    char datagram[516];
+
+    memcpy(&datagram[0], &opcode, 2);
+    memcpy(&datagram[2], &blockno, 2);
+    read(fd, data, sizeof(data));
+    snprintf(&datagram[4], sizeof(data), "%s", data);
+    // printf("%d", datagram[0]);
+    for (int i = 0; i < 516; i++) {
+        printf("%x ", datagram[i]);
+    }
+    
+    
+    /* Send datagram */
+    if (sendto(sockfd, &datagram, sizeof(datagram), 0, 
+        (struct sockaddr *) &client, (socklen_t) sizeof(client)) == -1)
+            printf("Error sending datagram\n");
+    
     return 0;
 }
